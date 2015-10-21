09149ccfc42e81d37f5b9cd2ac550de46d06a41e
Anandan <Anandan>
2015-10-21 18:57:44 -0500

    Anandan addded index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        
+    </head>
+
+    <body>
+<div align="center">
+                <h1>Hello Anandan, Joe, Raj and Tom</h1>
+                <h2>Congratulations! You have successfully installed your application on AWS using OpsWorks!</h2>
+                <p>Your Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
+                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+        </div>
+    </body>
+
+</html>
