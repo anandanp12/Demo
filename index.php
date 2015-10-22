85e206abc5b8e5c284a082d8d381a2402739050b
Anandan <Anandan>
2015-10-22 09:34:51 -0500

    Anandan added version 2.0

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <body>
 <div align="center">
                 <h1>Hello Anandan, Joe, Raj and Tom</h1>
-                <h2>Congratulations! You have successfully installed your Application Version 1.0 on AWS using OpsWorks!</h2>
+                <h2>Congratulations! You have successfully installed your Application Version 2.0 on AWS using OpsWorks!</h2>
                 <p>Your Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
         </div>
