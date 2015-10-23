3775465aa3b3b0570925893d6334bf007a0dbc07
Anandan <Anandan>
2015-10-23 11:13:15 -0500

    Changed to Elastic Bean Stalk

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     <body>
 <div align="center">
                 <h1>Hello Anandan, Joe, Raj and Tom</h1>
-                <h2>Congratulations! You have successfully installed your Application Version 2.5 on AWS using OpsWorks!</h2>
-                <p>Your Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
+                <h2>Congratulations! You have successfully installed your Application Version 2.5 on AWS using Elastic Bean Stalk!</h2>
+                <p>Your Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in Elastic Bean Stalk.</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
         </div>
     </body>
