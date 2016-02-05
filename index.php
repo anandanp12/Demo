18c28f48bd19a03c88b470611dedefe3018004a0
Anandan Palaniswamy <anandan.palaniswamy@aa.com>
2016-02-05 14:10:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 
     <body>
 <div align="center">
-                <h1>Hello Anandan, Joe, Raj and Tom</h1>
-                <h2>Congratulations! You have successfully installed your Application Version 2.5 on AWS using Elastic Bean Stalk!</h2>
+                <h1>Hello Architecture and Governance Teams at AA</h1>
+                <h2>Congratulations! You have successfully installed your Application Version 2.5 on Azure PAAS using CI/CD pipeline!</h2>
                 <p>Your Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in Elastic Bean Stalk.</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
         </div>
