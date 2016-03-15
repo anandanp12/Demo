399b41263a17e848ceb1ecbbed0974562ce77fe4
Anandan Palaniswamy <anandan.palaniswamy@aa.com>
2016-03-15 15:59:05 +0000

    Commit from Bluemix

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
 
     <body>
 <div align="center">
-                <h1>Hello Architecture and Governance Teams at AA</h1>
-                <h2>Congratulations! You have successfully installed your Application Version 2.5 on Azure PAAS using CI/CD pipeline!</h2>
-                <p>Your Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in Elastic Bean Stalk.</p>
+                <h1>Hello Cloud Team at AA</h1>
+                <h2>Congratulations! You have successfully installed your Application Version 1.0 on Bluemix using OneOps....sorry DevOps Services!</h2>
+                <p>Your Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a Bluemix Container.</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
         </div>
     </body>
