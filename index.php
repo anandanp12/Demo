3f2efc203d1e93cae6940f82624925526175d736
Anandan Palaniswamy <anandan.palaniswamy@aa.com>
2016-03-15 16:06:04 +0000

    Commit 3 from bluemix

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <div align="center">
                 <h1>Hello Clod Team at AA</h1>
                 <h2>Congratulations! You have successfully installed your Application Version 1.0 on Bluemix using OneOps....sorry DevOps Services!</h2>
-                <p>Your Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a Azure Container.</p>
+                <p>Your Application is now running on the host &ldquo;<?php echo gethostnme(); ?>&rdquo; in a Azure Container.</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
         </div>
     </body>
